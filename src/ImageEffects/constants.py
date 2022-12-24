75fab018dd9a0112f53dc67d69f41af5f4c20f21
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2022-12-24 19:50:32 +0530

    adding emojis to constants

--- src/ImageEffects/constants.py
+++ src/ImageEffects/constants.py
@@ -8,3 +8,4 @@ SRC_DIR = os.path.split(IMAGE_EFFECTS_DIR)[0]
 ROOT_DIR = os.path.split(SRC_DIR)[0]
 RESOURCES_DIR = os.path.join(ROOT_DIR, "resources")
 FONTS_DIR = os.path.join(RESOURCES_DIR, 'fonts')
+EMOJIS_DIR = os.path.join(RESOURCES_DIR, 'emojis')
