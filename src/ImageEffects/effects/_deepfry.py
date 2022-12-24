7a76799e7cceba8a2d7a98aa161571190dd10cbe
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2022-12-24 18:14:29 +0530

    changing constrast, and values

--- src/ImageEffects/effects/_deepfry.py
+++ src/ImageEffects/effects/_deepfry.py
@@ -4,10 +4,11 @@ from PIL import Image, ImageEnhance
 
 class _deepfry:
     '''Static class'''
-    LOWER_RESOLUTION = (512, 512)
+    LOWER_RESOLUTION = (256, 256)
     SATURATION_ENHANCE_SCALE = 2.0
-    RED_BAND_INCREASE_RATIO = 10.0
-    BLUE_BAND_INCREASE_RATIO = 0.8
+    RED_BAND_INCREASE_RATIO = 3.0
+    BLUE_BAND_INCREASE_RATIO = 0.5
+    CONTRAST_INCREASE_FACTOR = 3.0
 
     @classmethod
     def renderimage(cls, image: str) -> Image.Image:
@@ -18,13 +19,17 @@ class _deepfry:
         im = imSmall.resize(im.size, Image.Resampling.NEAREST)
 
         # increasing saturation
-        _converter = ImageEnhance.Color(im)
-        im = _converter.enhance(cls.SATURATION_ENHANCE_SCALE)
+        _converter_saturation = ImageEnhance.Color(im)
+        im = _converter_saturation.enhance(cls.SATURATION_ENHANCE_SCALE)
 
         # increasing red band's saturation.
         r, g, b = im.split()
         r = r.point(lambda i: i * cls.RED_BAND_INCREASE_RATIO)
         b = b.point(lambda i: i * cls.BLUE_BAND_INCREASE_RATIO)
         im = Image.merge('RGB', (r, g, b))
+
+        # increasing constrast
+        _converter_contrast = ImageEnhance.Contrast(im)
+        im = _converter_contrast.enhance(cls.CONTRAST_INCREASE_FACTOR)
 
         return im
