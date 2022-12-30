cfb6f4cfcb7911840b4c56dda5c904f5a67e9b69
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2022-12-30 23:03:20 +0530

    moving text_formatter to its own file

--- ImageEffects/ImageEffects/effects/_caption2.py
+++ ImageEffects/ImageEffects/effects/_caption2.py
@@ -2,6 +2,7 @@
 from PIL import Image, ImageDraw, ImageFont
 from importlib import resources
 import io
+from ImageEffects.utils import format_text
 
 
 class _caption2:
@@ -16,7 +17,7 @@ class _caption2:
         if len(text) == 0:
             text = 'text here'
 
-        text = cls.format_text(text)
+        text = format_text(cls.LINE_LENGTH, text)
 
         with resources.open_binary('resources.fonts', 'impact.ttf') as fp:
             _font_file = io.BytesIO(fp.read())
@@ -30,32 +31,3 @@ class _caption2:
         editable_im.text(((im.width - _w)/2, im.height - _h - 5), text, font=FONT, fill='white')
 
         return im
-
-    @classmethod
-    def format_text(cls, text: str) -> str:
-        '''takes the input string and formats if its longer than the threshold length'''
-        if len(text) > cls.LINE_LENGTH:
-
-            _text_list = []
-            _temp_length = 0
-            _temp_list = []
-
-            _word_list = text.split(' ')
-
-            for _each_word in _word_list:
-                _temp_length += len(_each_word)
-                _temp_list.append(_each_word)
-
-                if _temp_length > cls.LINE_LENGTH:
-                    _text_list.append(" ".join(_temp_list))
-                    _temp_list = []
-                    _temp_length = 0
-
-            if len(_temp_list) > 0:
-                _text_list.append(' '.join(_temp_list))
-
-            _text = '\n'.join(_text_list)
-
-            return _text
-
-        return text
